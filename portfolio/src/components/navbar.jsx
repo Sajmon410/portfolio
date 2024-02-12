e762d2c882c1e8a5ee963844bc30fcdeb6d607c7
unknown <simon.fhz@gmail.com>
2024-02-13 00:06:19 +0100

    Responsive started

--- portfolio/src/components/navbar.jsx
+++ portfolio/src/components/navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import './navbar.css';
 import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { IoMenu } from "react-icons/io5";
+
 const Navbar = () => {
-    
+  
+  const [showNavbar, setShowNavbar] = React.useState(false);
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+    console.log("idemo")
+  }
   return (
     <div className="navbar">
       <div className="content">
@@ -45,8 +52,49 @@ const Navbar = () => {
         >
                <CustomLink to="/contact">Contact</CustomLink>
         </motion.div>
+        
+
         </div>
-     
+        {showNavbar &&
+        <div id='nav-elements' className="animate__animated animate__fadeInRight">
+          <ul>
+            <li>
+            <motion.div
+            
+            whileHover={{ scale: 1.1 }}
+            whileTap={{rotate: 15 , scale: 1.5 }}
+            className="logo"
+            animate={{ rotate:0.1 }}
+          >
+              <Link to="/portfolio">
+          Home
+        </Link>
+          </motion.div>
+            </li>
+            <li>
+            <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{rotate: 15 , scale: 1.5 }}
+          className="menu"
+          animate={{ rotate:0.1 }}
+        >
+            <CustomLink to="/aboutme">About Me</CustomLink>
+        </motion.div>
+            </li>
+            <li>
+            <motion.div
+          whileHover={{ scale: 1.1 }}
+          whileTap={{rotate: 15 , scale: 1.5 }}
+          className="menu"
+          animate={{ rotate:0.1 }}
+        >
+               <CustomLink to="/contact">Contact</CustomLink>
+        </motion.div>
+            </li>
+          </ul>
+        </div>
+        }
+       <IoMenu onClick={handleShowNavbar} className='menu-icon'/>
     </div>
   );
 };

b41e81160a7dfe508ac308e47015cfe495479311
Sajmon410 <simon.fhz@gmail.com>
2024-03-13 23:05:54 +0100

    Final

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,19 @@
-import './App.css';
-import Navbar from './components/navbar.jsx'
-import Hero from './components/hero.jsx'
-import AboutMe from './components/aboutme.jsx'
-import Contact from './components/contact.jsx'
-import { Route, Routes } from "react-router-dom"
+import "./App.css";
+import Navbar from "./components/navbar.jsx";
+import Hero from "./components/hero.jsx";
+import AboutMe from "./components/aboutme.jsx";
+import Contact from "./components/contact.jsx";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-  <>
-  <Navbar/>
-  <Routes>
-    <Route path="/portfolio" element={<Hero />} />
-    <Route path="/aboutme" element={<AboutMe />} />
-    <Route path="/contact" element={<Contact />} />
-  </Routes>
-  </>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/portfolio" element={<Hero />} />
+        <Route path="/aboutme" element={<AboutMe />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 

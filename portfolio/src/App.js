fb1d2f18171ad2e1d3973e76af538a4a18706dbf
Simon Radosavljević <simon.fhz@gmail.com>
2025-02-19 02:13:35 +0100

    Reload fix

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,17 +3,18 @@ import Navbar from "./components/navbar.jsx";
 import Hero from "./components/hero.jsx";
 import AboutMe from "./components/aboutme.jsx";
 import Contact from "./components/contact.jsx";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
       <Routes>
         <Route path="/portfolio" element={<Hero />} />
         <Route path="/aboutme" element={<AboutMe />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    </Router>
   );
 }
 
